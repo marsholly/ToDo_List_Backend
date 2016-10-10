19be55f2be75bf8653ad3f22794dd90b72ddb10a
HONG ZHOU <holly_zhou@yahoo.com>
2016-10-10 09:40:51 -0700

    finished getAll and create new one

---
+++ app.js
@@ -0,0 +1,89 @@
+'use strict'
+const PORT = 8000;
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const TodoList = require('./models/todoList');
+const app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get('/todos', (req, res) => {
+  TodoList.getAllTodoLists((err, todos) => {
+    if(err) return res.status(400).send(err);
+    res.send(todos);
+  });
+});
+
+app.post('/todos', (req, res) => {
+  TodoList.createNewTodo(req.body, err => {
+    if(err) return res.status(400).send(err);
+    res.send('success')
+  })
+})
+
+//
+// // create a question
+// app.post('/question', (req, res) => {
+//   FlashCard.createQuestion(req.body, err => {
+//     if(err) return res.status(400).send(err);
+//     res.send('success');
+//   });
+// });
+//
+// // delete a question
+// app.delete('/question/:id',(req, res) => {
+//   let _id = req.params.id;
+//   FlashCard.removeOneQuestion(_id, err => {
+//     if(err) return res.status(400).send(err);
+//     res.send('done');
+//   });
+// });
+//
+// // update a question
+// app.put('/question/:id', (req, res) => {
+//   let _id = req.params.id;
+//   FlashCard.updateOneQuestion(_id, req.body, (err, question) => {
+//     if(err) return res.status(400).send(err);
+//     res.send(question);
+//   });
+// })
+//
+// //  get a category question  /question/category
+// app.get('/question/:category',(req, res) => {
+//   FlashCard.getAllQuestions((err, questions) => {
+//     if(err) {
+//       return res.status(400).send(err);
+//     }
+//     let category = req.params.category;
+//     let newQuestionArr = questions.filter(question => {
+//       return question.category === category;
+//     })
+//     let total = newQuestionArr.length;
+//     let index = Math.floor(Math.random() * total);
+//     let questionObj = newQuestionArr[index];
+//     id = questionObj.id;
+//     let question = `[ ${questionObj.category} ] Q: ${questionObj.question}`;
+//     res.send(question);
+//   })
+// });
+//
+// //  get answer  /answer
+// app.get('/answer',(req, res) => {
+//   FlashCard.getOneQuestion(id, (err, question) => {
+//     if(err) {
+//       return res.status(400).send(err);
+//     }
+//     let answer =`The answer is: ${question.answer}`;
+//     res.send(answer);
+//   })
+// });
+//
+
+
+app.listen(PORT, err => {
+  console.log(err || `Express listening on port ${PORT}`);
+});
---
+++ models/todoList.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+const todoListPath = path.join(__dirname, '../data/todoList.json');
+const uuid = require('uuid');
+
+exports.getAllTodoLists = function(cb) {
+  fs.readFile(todoListPath, (err, buffer) => {
+    if (err) return cb(err);
+    let data;
+    try {
+      data = JSON.parse(buffer);
+    } catch(e) {
+      data = [];
+    }
+    cb(null, data);
+  });
+}
+
+exports.write = function(newData, cb) {
+  let json = JSON.stringify(newData);
+  fs.writeFile(todoListPath, json, cb);
+}
+
+exports.createNewTodo = function(newTodo, cb) {
+  exports.getAllTodoLists((err, todos) => {
+    if(err) return cb(err);
+    newTodo.id = uuid();
+    newTodo.isComplete = false;
+    todos.push(newTodo);
+    exports.write(todos, cb);
+  });
+}
